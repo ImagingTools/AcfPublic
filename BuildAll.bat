2bbb626bc12709b23f7fea60dc0f123fc1ed7b97
wg <wg@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2011-11-14 15:05:42 +0000

    Old build scripts removed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@150 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- BuildAll.bat
+++
@@ -1,7 +0,0 @@
-cd %ACFPUBLICDIR%
-
-MsBuild.exe %ACFPUBLICDIR%\Acf\Impl\VC9\AcfAll.sln
-MsBuild.exe %ACFPUBLICDIR%\AcfSln\Impl\VC9\AcfSlnAll.sln
-MsBuild.exe %ACFPUBLICDIR%\Iacf\Impl\VC9\IacfAll.sln
-
-pause
