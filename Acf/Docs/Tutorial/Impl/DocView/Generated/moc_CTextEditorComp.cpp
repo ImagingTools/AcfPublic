c0ea8eb99141dc01dd65f6c3b0da51b2c59f8944
wg <wg@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2011-12-11 16:46:30 +0000

    Garbages removed.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@188 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Docs/Tutorial/Impl/DocView/Generated/moc_CTextEditorComp.cpp
+++ Acf/Docs/Tutorial/Impl/DocView/Generated/moc_CTextEditorComp.cpp
@@ -1,30 +1,8 @@
-/********************************************************************************
-**
-**	Copyright (c) 2007-2010 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.imagingtools.de, write info@imagingtools.de or contact
-**  by Skype to ACF_infoline for further information about the ACF.
-**
-********************************************************************************/
-
-
 /****************************************************************************
 ** Meta object code from reading C++ file 'CTextEditorComp.h'
 **
-** Created: Mon 28. Mar 22:23:59 2011
-**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.1)
+** Created: Fri 18. Nov 11:01:31 2011
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -33,7 +11,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'CTextEditorComp.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 62
-#error "This file was generated using the moc from 4.7.1. It"
+#error "This file was generated using the moc from 4.7.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
