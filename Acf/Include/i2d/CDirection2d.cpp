f928fe450e1fb02d8d77934fbd0ee5e0f1b7375f
quiss <quiss@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2012-01-26 15:16:00 +0000

    i2d::CRect and i2d::CDirection2d moved from QSF
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@240 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Acf/Include/i2d/CDirection2d.cpp
@@ -0,0 +1,194 @@
+#include "i2d/CDirection2d.h"
+
+
+// ACF includes
+#include "imath/imath.h"
+
+
+namespace i2d
+{
+
+
+CDirection2d::CDirection2d()
+	:	m_direction(0.0)
+{
+	I_ASSERT(Invariant());
+}
+
+
+CDirection2d::CDirection2d(const CDirection2d& direction)
+:	m_direction(direction.m_direction)
+{
+	I_ASSERT(direction.Invariant());
+
+	I_ASSERT(Invariant());
+}
+
+
+CDirection2d::CDirection2d(double radian)
+	:	m_direction(radian)
+{
+	I_ASSERT(radian >= -I_PI - i2d_EPSYLON);
+	I_ASSERT(radian <= I_PI + i2d_EPSYLON);
+
+	I_ASSERT(Invariant());
+}
+
+
+double CDirection2d::DistInRadian(const CDirection2d& dir2) const
+{
+	I_ASSERT(Invariant());
+	I_ASSERT(dir2.Invariant());
+
+	const double w1( istd::Min(m_direction, dir2.m_direction )); 
+	const double w2( istd::Max(m_direction, dir2.m_direction ));
+
+	const double dist = istd::Min(w2-w1, I_2PI - (w2-w1));
+	
+	I_ASSERT(dist >= 0.0);
+	I_ASSERT(dist <= I_PI);
+		
+	return dist;
+}
+
+
+double CDirection2d::DistInDegree(const CDirection2d& dir2) const
+{
+	I_ASSERT(Invariant());
+	I_ASSERT(dir2.Invariant());
+
+	const double dist(DistInRadian(dir2) / I_2PI * 360.0);
+
+	I_ASSERT(dist >= 0.0);
+	I_ASSERT(dist <= 180.0);
+		
+	return dist;
+}
+
+
+bool CDirection2d::operator==(const CDirection2d& dir2) const
+{
+	I_ASSERT(Invariant());
+	I_ASSERT(dir2.Invariant());
+
+	return fabs(DistInRadian(dir2)) < I_BIG_EPSILON;
+}
+
+
+bool CDirection2d::Invariant() const
+{
+	return (m_direction >= -I_PI - I_BIG_EPSILON) && (m_direction <= I_PI + I_BIG_EPSILON);
+}
+
+
+double CDirection2d::Normalize(double radian)
+{
+	double retVal = (radian >= 0)?
+					::fmod(radian + I_PI, I_2PI) - I_PI:
+					I_PI - ::fmod(I_PI - radian, I_2PI);
+					
+	I_ASSERT((retVal >= -I_PI - I_BIG_EPSILON) && (retVal <= I_PI + I_BIG_EPSILON));
+
+	return retVal;
+}
+
+
+CDirection2d CDirection2d::FromRadian(double radian)
+{
+	return CDirection2d(CDirection2d::Normalize(radian));
+}
+
+
+CDirection2d CDirection2d::FromDegree(double degree)
+{
+	return CDirection2d(CDirection2d::Normalize(degree * (I_2PI / 360.0)));
+}
+
+
+double CDirection2d::ToDegree() const
+{
+	return fmod(m_direction / I_2PI * 360.0 + 360.0, 360.0);
+}
+
+
+double CDirection2d::ToRadian() const
+{
+	return m_direction;
+}
+
+
+CDirection2d CDirection2d::Avg(const CDirection2d& dir2) const
+{
+	double dist(DistInRadian(dir2) / 2.0);
+	
+	CDirection2d res1(this->TurnByRadian(dist));
+	CDirection2d res2(this->TurnByRadian(-dist));
+
+	CDirection2d& res = dir2.DistInRadian(res1) <= dir2.DistInRadian(res2) ? res1 : res2;	
+
+	I_ASSERT(res.DistInRadian(*this) + res.DistInRadian(dir2) == DistInRadian(dir2));
+
+	return res;
+}
+
+
+CDirection2d CDirection2d::Avg(double weight, const CDirection2d& dir2) const
+{
+	I_ASSERT(Invariant());
+	I_ASSERT(dir2.Invariant())
+	I_ASSERT(weight >= 0.0);
+	I_ASSERT(weight <= 1.0);
+
+	double dist(DistInRadian(dir2) * weight);
+	
+	CDirection2d res1(dir2.TurnByRadian(dist));
+	CDirection2d res2(dir2.TurnByRadian(-dist));
+
+	CDirection2d& res = DistInRadian(res1) <= DistInRadian(res2) ? res1 : res2;
+
+	I_IF_DEBUG(
+		const double a(res.DistInRadian(*this));
+		const double b(res.DistInRadian(dir2));
+		const double c(DistInRadian(dir2));
+		I_ASSERT((a + b < c + i2d_EPSYLON) && (a + b > c - i2d_EPSYLON));
+	);
+	
+	I_ASSERT(res.Invariant());
+
+	return res;
+}
+
+
+CDirection2d CDirection2d::TurnByRadian(double turn) const
+{
+	I_ASSERT(Invariant()); 
+
+	const CDirection2d& res = CDirection2d::FromRadian(this->ToRadian()+turn);
+
+	I_ASSERT(res.Invariant());
+
+	return res;
+}
+
+
+bool CDirection2d::IsInRange(const CDirection2d& leftDirectionLimit, const CDirection2d& rightDirectionLimit) const
+{
+	const double wl(leftDirectionLimit.ToRadian());
+	const double wr(rightDirectionLimit.ToRadian());
+	const double wt(ToRadian());
+
+	if (wl <= wt && wt <= wr){
+		return true;
+	}
+	else if ( wl > wr && (wt <= wr || wt >= wl )){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+} // namespace i2d
+
+
---
+++ Acf/Include/i2d/CDirection2d.h
@@ -0,0 +1,101 @@
+#ifndef i2d_CDirection2d2d_included
+#define i2d_CDirection2d_included
+
+
+namespace i2d
+{
+
+
+/**
+	Represents a direction in 2D space
+*/
+class CDirection2d
+{
+public:
+	CDirection2d();
+
+	/**	Construct a new object by copy.
+	 */
+	CDirection2d(const CDirection2d& direction);
+
+	/**	Construct a new CDirection2d object and initialize from radian.
+	 */
+	static CDirection2d FromRadian(double radian);
+
+	/**	Construct a new CDirection2d object and initialize from degree.
+	 */
+	static CDirection2d FromDegree(double degree);
+
+	/**	Get direction in degrees.
+	 *		@return	the normalized direction in range [0, 360).
+	 */
+	double ToDegree() const;
+
+	/**	Get direction in radians.
+	 *		@return	the normalized direction in range [-PI, +PI].
+	 */
+	double ToRadian() const;
+
+	/**	Get angle difference in radians.
+	 *		@return	the minimal angle distance between the this
+	 *				and dir2 directions in radians [0, +PI].
+	 */
+	double DistInRadian(const CDirection2d& dir2) const;
+
+	/**	Get angle difference in degrees.
+	 *		@return the minimal angle distance between the this
+	 *				and dir2 directions in degree [0, +180].
+	 */
+	double DistInDegree(const CDirection2d& dir2) const;
+
+	/**	Get minimal distance between two directions.
+	 *		@return	the direction that minimizes the distance to this and dir2.
+	 */
+	CDirection2d Avg(const CDirection2d& dir2) const;
+
+	/**	Get weighted minimal distance between two directions.
+	 *		@return	the direction that minimizes the weighted distances between both directions.
+	 */
+	CDirection2d Avg(double weight, const CDirection2d& dir2) const;
+
+
+	CDirection2d TurnByRadian(double turn) const;
+
+	/**	Compare the objects direction with the one of dir2.
+	 *		@param	dir2	direction to compare.
+	 *		@return			true if both are equal.
+	 */
+	bool operator==(const CDirection2d& dir2) const;
+
+	/**
+	 * Check if the direction is in the direction range between leftDirectionLimit and rightDirectionLimit
+	 */
+	bool IsInRange(const CDirection2d& leftDirectionLimit, const CDirection2d& rightDirectionLimit) const;
+
+private:
+	/**	Construct a new object from radian coordinates.
+	 *		@param	radian	angle [-PI, +PI]
+	 */
+	explicit CDirection2d(double radian);
+
+	/**	The direction in radian.
+	 */
+	double m_direction;
+
+	/**	Checks the objects invariant. This is for assertion annotation
+	 *		@return		true if all object invariants are valid
+	 */
+	bool Invariant() const;
+
+	/**	Normalize internal angle to the range [-PI, +PI]
+	 */
+	static double Normalize(double radian);
+};
+
+
+} // namespace i2d
+
+
+#endif // !i2d_CDirection2d_included
+
+
