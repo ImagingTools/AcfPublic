3a052f6611b5a8222ea8e8a493ef17619a855dc1
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2012-05-25 09:23:31 +0000

    Automatic update from Build Server ACF version 1.6.0.62 (2343) ACF-Solutions version 0.5.0.199 (555) IACF version 0.3.2.158 (506)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@371 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iview/CPolygonCalibrationShape.cpp
+++ Acf/Include/iview/CPolygonCalibrationShape.cpp
@@ -345,10 +345,10 @@ void CPolygonCalibrationShape::DrawCurve(QPainter& drawContext) const
 
 				point1 = point2;
 			}
+
+			drawContext.restore();
 		}
 	}
-
-	drawContext.restore();
 }
 
 
@@ -378,6 +378,8 @@ void CPolygonCalibrationShape::DrawArea(QPainter& drawContext) const
 				calib.GetInvPositionAt(polygon.GetNode(i), viewPos);
 				m_screenPoints[i] = iqt::GetQPoint(transform.GetScreenPosition(viewPos));
 			}
+
+			drawContext.save();
 
 			if (IsSelected()){
 				drawContext.setBrush(colorShema.GetBrush(IColorShema::SB_HALF_TRANSPARENT));
