528b932cc66e083be9af14f6f96a263e23c9ab7b
quiss_pl <quiss_pl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2012-09-14 14:50:59 +0000

    CComposedParamsSetGuiComp: fixed removing editors when a model is detached with DT_SIMPLE
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@552 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iqtprm/CComposedParamsSetGuiComp.cpp
+++ Acf/Include/iqtprm/CComposedParamsSetGuiComp.cpp
@@ -1,23 +1,23 @@
 /********************************************************************************
-**
-**	Copyright (C) 2007-2011 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.imagingtools.de, write info@imagingtools.de or contact
-**	by Skype to ACF_infoline for further information about the ACF.
-**
-********************************************************************************/
+ **
+ **	Copyright (C) 2007-2011 Witold Gantzke & Kirill Lepskiy
+ **
+ **	This file is part of the ACF Toolkit.
+ **
+ **	This file may be used under the terms of the GNU Lesser
+ **	General Public License version 2.1 as published by the Free Software
+ **	Foundation and appearing in the file LicenseLGPL.txt included in the
+ **	packaging of this file.  Please review the following information to
+ **	ensure the GNU Lesser General Public License version 2.1 requirements
+ **	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+ **
+ **	If you are unsure which license is appropriate for your use, please
+ **	contact us at info@imagingtools.de.
+ **
+ ** 	See http://www.imagingtools.de, write info@imagingtools.de or contact
+ **	by Skype to ACF_infoline for further information about the ACF.
+ **
+ ********************************************************************************/
 
 
 #include "iqtprm/CComposedParamsSetGuiComp.h"
@@ -49,13 +49,14 @@ namespace iqtprm
 
 
 CComposedParamsSetGuiComp::CComposedParamsSetGuiComp()
-:	m_currentGuiIndex(-1),
-	m_guiContainerPtr(NULL)
+: m_currentGuiIndex(-1),
+m_guiContainerPtr(NULL)
 {
 }
 
 
 // reimplemented (imod::IModelEditor)
+
 
 void CComposedParamsSetGuiComp::UpdateModel() const
 {
@@ -106,6 +107,7 @@ void CComposedParamsSetGuiComp::UpdateEditor(int updateFlags)
 
 // reimplemented (iqtgui::CGuiComponentBase)
 
+
 void CComposedParamsSetGuiComp::OnGuiCreated()
 {
 	DesignType guiMode = DesignType(*m_designTypeAttrPtr);
@@ -129,25 +131,25 @@ void CComposedParamsSetGuiComp::OnGuiCreated()
 	QToolBox* toolBoxPtr;
 	QTabWidget* tabWidgetPtr;
 	switch (guiMode){
-	case DT_TAB_WIDGET:
-		ParamsFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
-		m_guiContainerPtr = tabWidgetPtr = new QTabWidget(ParamsFrame);
-		QObject::connect(tabWidgetPtr, SIGNAL(currentChanged(int)), this, SLOT(OnEditorChanged(int)));
-		layoutPtr->addWidget(tabWidgetPtr);
-		m_currentGuiIndex = 0;
-		break;
+		case DT_TAB_WIDGET:
+			ParamsFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+			m_guiContainerPtr = tabWidgetPtr = new QTabWidget(ParamsFrame);
+			QObject::connect(tabWidgetPtr, SIGNAL(currentChanged(int)), this, SLOT(OnEditorChanged(int)));
+			layoutPtr->addWidget(tabWidgetPtr);
+			m_currentGuiIndex = 0;
+			break;
 
-	case DT_TOOL_BOX:
-		ParamsFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
-		m_guiContainerPtr = toolBoxPtr = new QToolBox(ParamsFrame);
-		QObject::connect(toolBoxPtr, SIGNAL(currentChanged(int)), this, SLOT(OnEditorChanged(int)));
-		layoutPtr->addWidget(toolBoxPtr);
-		m_currentGuiIndex = 0;
-		break;
+		case DT_TOOL_BOX:
+			ParamsFrame->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+			m_guiContainerPtr = toolBoxPtr = new QToolBox(ParamsFrame);
+			QObject::connect(toolBoxPtr, SIGNAL(currentChanged(int)), this, SLOT(OnEditorChanged(int)));
+			layoutPtr->addWidget(toolBoxPtr);
+			m_currentGuiIndex = 0;
+			break;
 
-	default:
-		m_guiContainerPtr = ParamsFrame;
-		m_currentGuiIndex = -1;
+		default:
+			m_guiContainerPtr = ParamsFrame;
+			m_currentGuiIndex = -1;
 	}
 
 	// map gui objects with their names (the container will be filled by OnGuiModelAttached())
@@ -191,6 +193,7 @@ void CComposedParamsSetGuiComp::OnGuiDestroyed()
 
 // reimplemented (iqt2d::IViewExtender)
 
+
 void CComposedParamsSetGuiComp::AddItemsToScene(iqt2d::IViewProvider* providerPtr, int flags)
 {
 	I_ASSERT(providerPtr != NULL);
@@ -212,6 +215,7 @@ void CComposedParamsSetGuiComp::RemoveItemsFromScene(iqt2d::IViewProvider* provi
 
 
 // protected methods
+
 
 void CComposedParamsSetGuiComp::AttachToScene(iqt2d::IViewProvider* providerPtr, int flags)
 {
@@ -285,6 +289,7 @@ void CComposedParamsSetGuiComp::DetachFromScene(iqt2d::IViewProvider* providerPt
 
 // reimplemented (imod::IObserver)
 
+
 void CComposedParamsSetGuiComp::OnGuiModelAttached()
 {
 	BaseClass::OnGuiModelAttached();
@@ -343,7 +348,7 @@ void CComposedParamsSetGuiComp::OnGuiModelAttached()
 					panelPtr = new QWidget(m_guiContainerPtr);
 					QLayout* panelLayoutPtr = new QVBoxLayout(panelPtr);
 					panelLayoutPtr->setContentsMargins(6, 0, 6, 0);
-					QToolBox* toolBoxPtr = static_cast<QToolBox*> (m_guiContainerPtr);
+					QToolBox* toolBoxPtr = static_cast<QToolBox*>(m_guiContainerPtr);
 					toolBoxPtr->addItem(panelPtr, name);
 
 					addSpacer = true;
@@ -387,14 +392,14 @@ void CComposedParamsSetGuiComp::OnGuiModelAttached()
 				QWidget* framePtr = guiWidgetPtr->parentWidget();
 				if (framePtr){
 					if (guiMode == DT_TAB_WIDGET){
-						QTabWidget* tabWidgetPtr = static_cast<QTabWidget*> (m_guiContainerPtr);
+						QTabWidget* tabWidgetPtr = static_cast<QTabWidget*>(m_guiContainerPtr);
 						int index = tabWidgetPtr->indexOf(framePtr);
 						if (index >= 0){
 							tabWidgetPtr->removeTab(index);
 						}
 					}
 					else if (guiMode == DT_TOOL_BOX){
-						QToolBox* toolBoxPtr = static_cast<QToolBox*> (m_guiContainerPtr);
+						QToolBox* toolBoxPtr = static_cast<QToolBox*>(m_guiContainerPtr);
 						int index = toolBoxPtr->indexOf(framePtr);
 						if (index >= 0){
 							toolBoxPtr->removeItem(index);
@@ -413,11 +418,11 @@ void CComposedParamsSetGuiComp::OnGuiModelAttached()
 
 	// restore selection
 	if (guiMode == DT_TAB_WIDGET){
-		QTabWidget* tabWidgetPtr = static_cast<QTabWidget*> (m_guiContainerPtr);
+		QTabWidget* tabWidgetPtr = static_cast<QTabWidget*>(m_guiContainerPtr);
 		tabWidgetPtr->setCurrentIndex(m_currentGuiIndex);
 	}
 	else if (guiMode == DT_TOOL_BOX){
-		QToolBox* toolBoxPtr = static_cast<QToolBox*> (m_guiContainerPtr);
+		QToolBox* toolBoxPtr = static_cast<QToolBox*>(m_guiContainerPtr);
 		toolBoxPtr->setCurrentIndex(m_currentGuiIndex);
 	}
 
@@ -444,13 +449,13 @@ void CComposedParamsSetGuiComp::OnGuiModelDetached()
 	// clear the gui container
 	int guiMode = *m_designTypeAttrPtr;
 	if (guiMode == DT_TAB_WIDGET){
-		QTabWidget* tabWidget = static_cast<QTabWidget*> (m_guiContainerPtr);
+		QTabWidget* tabWidget = static_cast<QTabWidget*>(m_guiContainerPtr);
 		for (int i = tabWidget->count() - 1; i >= 0; i--){
 			tabWidget->removeTab(i);
 		}
 	}
 	else if (guiMode == DT_TOOL_BOX){
-		QToolBox* toolBox = static_cast<QToolBox*> (m_guiContainerPtr);
+		QToolBox* toolBox = static_cast<QToolBox*>(m_guiContainerPtr);
 		for (int i = toolBox->count() - 1; i >= 0; i--){
 			toolBox->removeItem(i);
 		}
@@ -458,8 +463,9 @@ void CComposedParamsSetGuiComp::OnGuiModelDetached()
 	else{
 		QLayout* layout = m_guiContainerPtr->layout();
 		for (int i = layout->count() - 1; i >= 0; i--){
-			layout->removeItem(layout->itemAt(i));
+			layout->itemAt(i)->widget()->hide();
 		}
+		qDeleteAll(layout->children());
 	}
 
 	m_connectedEditorsMap.clear();
@@ -485,6 +491,7 @@ void CComposedParamsSetGuiComp::OnGuiModelDetached()
 
 
 // protected slots
+
 
 void CComposedParamsSetGuiComp::OnEditorChanged(int index)
 {
