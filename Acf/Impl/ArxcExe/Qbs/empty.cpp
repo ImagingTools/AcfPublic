e7e9999dc83db442244b587a13d460507df24ded
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2013-08-14 07:40:53 +0000

    Automatic update from Build Server ACF version 1.6.5.294 (3105) ACF-Solutions version 0.6.0.149 (993) IACF version 0.3.4.24 (612)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@949 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Impl/ArxcExe/Qbs/empty.cpp
+++ Acf/Impl/ArxcExe/Qbs/empty.cpp
@@ -20,25 +20,3 @@
 ********************************************************************************/
 
 
-#ifndef istd_AcfVersion_included
-#define istd_AcfVersion_included
-
-
-namespace istd
-{
-
-
-enum RepositoryState
-{
-	RS_ORIGINAL_VERSION =  3105,
-	RS_DIRTY_FLAG = 0,
-	RS_USE_VERSION = RS_ORIGINAL_VERSION + RS_DIRTY_FLAG
-};
-
-
-} // namespace istd
-
-
-#endif // !istd_AcfVersion_included
-
-
