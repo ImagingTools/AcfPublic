54534c6037f9e43973a91ab93023b026403d8852
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2013-12-17 16:43:50 +0000

    Automatic update from Build Server ACF version 1.7.0.34 (3263) ACF-Solutions version 0.6.1.12 (1072) IACF version 0.4.0.2 (633)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1066 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Config/Windows/RegisterEnvVariables.js
+++ AcfSln/Config/Windows/RegisterEnvVariables.js
@@ -1,29 +1,7 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2013 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org, write info@imagingtools.de or contact
-**	by Skype to ACF_infoline for further information about the ACF.
-**
-********************************************************************************/
-
-
 var variableExt = "envVariable"
 var variableExp = new RegExp(".*\." + variableExt + "$");
 var scriptExp = new RegExp(".*\.autoregister\.js$");
-var registryPath = "HKEY_CURRENT_USER\\Environment\\";
+var registryPath = "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\\";
 
 
 function ProcessFolder(shell, folder)
@@ -57,7 +35,7 @@ function ProcessFolder(shell, folder)
 
 
 var fileSystem = WScript.CreateObject("Scripting.FileSystemObject");
-var fullPath = fileSystem.GetParentFolderName(WScript.ScriptFullName);
+var fullPath = fileSystem.GetParentFolderName(fileSystem.GetParentFolderName(fileSystem.GetParentFolderName(WScript.ScriptFullName)));
 var shell = WScript.CreateObject("WScript.Shell");
 var message = ProcessFolder(shell, fileSystem.GetFolder(fullPath));
 if (message.length > 0){
