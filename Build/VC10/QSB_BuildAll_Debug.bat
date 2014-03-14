fcfd24faef67cbe6420966a4a5c8e9c6787f1b21
wg <wg@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2014-03-14 13:25:21 +0000

    QBS: Build scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1149 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Build/VC10/QSB_BuildAll_Debug.bat
+++ Build/VC10/QSB_BuildAll_Debug.bat
@@ -1,10 +1,10 @@
 echo off
 
-cd /d %~dp0\..\..
+cd /d %~dp0..\..
 
 call "%VS100COMNTOOLS%vsvars32.bat" x86
 
-qbs build -f %~dp0/../Qbs/AcfWorldAll.qbs debug profile:qtc_VC10
-qbs install -f %~dp0/../Qbs/AcfWorldAll.qbs --install-root %~dp0/../../Acf --no-build debug profile:qtc_VC10
+qbs build -f %~dp0../Qbs/AcfWorldWorkspace.qbs debug profile:qtc_VC10
+qbs install -f %~dp0../Qbs/AcfWorldWorkspace.qbs --install-root %~dp0../../Acf --no-build debug profile:qtc_VC10
 
 pause
--- Build/VC9/QBS_BuildAll_Release.bat
+++ Build/VC9/QBS_BuildAll_Release.bat
@@ -1,10 +1,10 @@
 echo off
 
-cd /d %~dp0\..\..
+cd /d %~dp0..\..
 
 call "%VS90COMNTOOLS%vsvars32.bat" x86
 
-qbs build -f %~dp0/../Qbs/AcfWorldAll.qbs release profile:qtc_VC9
-qbs install -f %~dp0/../Qbs/AcfWorldAll.qbs --install-root %~dp0/../../Acf --no-build release profile:qtc_VC9
+qbs build -f %~dp0../Qbs/AcfWorldWorkspace.qbs release profile:qtc_VC9
+qbs install -f %~dp0../Qbs/AcfWorldWorkspace.qbs --install-root %~dp0../../Acf --no-build release profile:qtc_VC9
 
 pause
