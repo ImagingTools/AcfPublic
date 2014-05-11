22fd4539704310644b0bfc56109d93982cbc01e1
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2014-05-11 04:36:14 +0000

    Automatic update from Build Server ACF version 1.7.0.247 (3476) ACF-Solutions version 0.6.1.77 (1137) IACF version 0.4.0.17 (648)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1217 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/ilog/CStreamLogCompBase.cpp
+++ Acf/Include/ilog/CStreamLogCompBase.cpp
@@ -96,7 +96,7 @@ void CStreamLogCompBase::WriteMessageToStream(const istd::IInformationProvider& 
 {
 	QString messageText = GenerateMessageText(message);
 	
-	WriteText(messageText + "/n", message.GetInformationCategory());
+	WriteText(messageText + "\n", message.GetInformationCategory());
 }
 
 
