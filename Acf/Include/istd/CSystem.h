06a067b7ba18c7acb916a1889833fefc0b224912
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-05-13 17:36:55 +0000

    Automatic update from Build Server ACF version 1.8.0.212 (3717) ACF-Solutions version 0.6.1.200 (1260) IACF version 0.4.0.44 (675)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1419 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/istd/CSystem.h
+++ Acf/Include/istd/CSystem.h
@@ -120,7 +120,7 @@ public:
 	static bool RemoveDirectory(const QString& directoryPath);
 
 	/**
-		Copy file to directory. When result directory doesn't exist create it.
+		Copy file to directory. When result directory doesn't exist, create it.
 	*/
 	static bool FileCopy(const QString& source, const QString& result, bool overWrite = false);
 
