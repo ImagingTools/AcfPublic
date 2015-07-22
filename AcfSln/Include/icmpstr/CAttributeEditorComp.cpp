2be12e321ca55b726ecad4bda175d62c6b6e250f
am <am@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-07-22 12:18:34 +0000

    potential crashes fixed
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1499 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/icmpstr/CAttributeEditorComp.cpp
+++ AcfSln/Include/icmpstr/CAttributeEditorComp.cpp
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (c) 2007-2014 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF-Solutions Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org, write info@imagingtools.de or contact
-**	by Skype to ACF_infoline for further information about the ACF-Solutions.
-**
-********************************************************************************/
-
-
 #include "icmpstr/CAttributeEditorComp.h"
 
 
@@ -423,6 +401,10 @@ void CAttributeEditorComp::UpdateGeneralView()
 
 void CAttributeEditorComp::UpdateAttributesView()
 {
+	if (!IsGuiCreated()){
+		return;
+	}
+
 	iqt::CSignalBlocker signalBlocker(AttributeTree);
 
 	m_attrInfosMap.clear();
@@ -556,6 +538,10 @@ void CAttributeEditorComp::UpdateAttributesView()
 
 void CAttributeEditorComp::UpdateInterfacesView()
 {
+	if (!IsGuiCreated()){
+		return;
+	}
+
 	iqt::CSignalBlocker signalBlocker(InterfacesTree);
 
 	bool hasWarning = false;
@@ -692,6 +678,10 @@ void CAttributeEditorComp::UpdateFlagsView()
 
 void CAttributeEditorComp::UpdateSubcomponentsView()
 {
+	if (!IsGuiCreated()){
+		return;
+	}
+
 	iqt::CSignalBlocker signalBlocker(ComponentsTree);
 
 	bool hasWarning = false;
