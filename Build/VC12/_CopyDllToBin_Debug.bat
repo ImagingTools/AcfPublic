f836701605c180b8905d8e1c48f0d40ac03beb05
wg <wg@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-09-16 09:20:24 +0000

    Scripts for DLL copying added to build folders.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1630 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Build/VC12/_CopyDllToBin_Debug.bat
@@ -0,0 +1,16 @@
+xcopy /Y %QTDIR%\bin\icu*.dll ..\..\Acf\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Cored.dll ..\..\Acf\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Guid.dll ..\..\Acf\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Widgetsd.dll ..\..\Acf\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Xmld.dll ..\..\Acf\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\icu*.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Cored.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Guid.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Widgetsd.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Xmld.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?Svgd.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\bin\Qt?PrintSupportd.dll ..\..\AcfSln\Bin\DebugVC12\
+xcopy /Y %QTDIR%\plugins\imageformats\qsvgd.dll ..\..\AcfSln\Bin\DebugVC12\imageformats\
+xcopy /Y %QTDIR%\plugins\iconengines\qsvgicond.dll ..\..\AcfSln\Bin\DebugVC12\iconengines\
+
+pause
---
+++ Build/VC12/_CopyDllToBin_Release.bat
@@ -0,0 +1,16 @@
+xcopy /Y %QTDIR%\bin\icu*.dll ..\..\Acf\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Core.dll ..\..\Acf\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Gui.dll ..\..\Acf\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Widgets.dll ..\..\Acf\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Xml.dll ..\..\Acf\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\icu*.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Core.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Gui.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Widgets.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Xml.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?Svg.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\bin\Qt?PrintSupport.dll ..\..\AcfSln\Bin\ReleaseVC12\
+xcopy /Y %QTDIR%\plugins\imageformats\qsvg.dll ..\..\AcfSln\Bin\ReleaseVC12\imageformats\
+xcopy /Y %QTDIR%\plugins\iconengines\qsvgicon.dll ..\..\AcfSln\Bin\ReleaseVC12\iconengines\
+
+pause
