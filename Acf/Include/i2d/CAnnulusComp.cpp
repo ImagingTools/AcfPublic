7469f840d38e810185f40c6f6f366cb643369eca
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-09-24 13:20:46 +0000

    Automatic update from Build Server ACF version 1.9.0.142 (3905) ACF-Solutions version 1.0.1.102 (1411) IACF version 0.4.0.74 (705)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1642 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/i2d/CAnnulusComp.cpp
+++ Acf/Include/i2d/CAnnulusComp.cpp
@@ -35,6 +35,14 @@ void CAnnulusComp::OnComponentCreated()
 {
 	BaseClass::OnComponentCreated();
 
+	ResetData();
+}
+
+
+// reimplemented (istd::IChangeable)
+
+bool CAnnulusComp::ResetData(CompatibilityMode /*mode*/)
+{
 	Q_ASSERT(m_centerXAttrPtr.IsValid());
 	Q_ASSERT(m_centerYAttrPtr.IsValid());
 	Q_ASSERT(m_innerRadiusAttrPtr.IsValid());
@@ -43,6 +51,8 @@ void CAnnulusComp::OnComponentCreated()
 	SetPosition(i2d::CVector2d(*m_centerXAttrPtr, *m_centerYAttrPtr));
 	SetOuterRadius(*m_outerRadiusAttrPtr);
 	SetInnerRadius(*m_innerRadiusAttrPtr);
+
+	return true;
 }
 
 
