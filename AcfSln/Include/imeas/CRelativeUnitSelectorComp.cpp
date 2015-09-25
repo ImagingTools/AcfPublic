0016310f742d44fe071a258da0ee76e722e1e2ad
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-09-25 11:10:59 +0000

    Automatic update from Build Server ACF version 1.9.0.144 (3907) ACF-Solutions version 1.0.1.104 (1413) IACF version 0.4.0.74 (705)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1645 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ AcfSln/Include/imeas/CRelativeUnitSelectorComp.cpp
@@ -0,0 +1,231 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF-Solutions Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#include "imeas/CRelativeUnitSelectorComp.h"
+
+
+// ACF includes
+#include "iser/CPrimitiveTypesSerializer.h"
+
+
+namespace imeas
+{
+
+
+// public methods
+
+CRelativeUnitSelectorComp::CRelativeUnitSelectorComp()
+	:m_displayMode(DM_PERCENT)
+{
+}
+
+
+// reimplemented (iprm::ISelectionParam)
+
+const iprm::IOptionsList* CRelativeUnitSelectorComp::GetSelectionConstraints() const
+{
+	return this;
+}
+
+
+int CRelativeUnitSelectorComp::GetSelectedOptionIndex() const
+{
+	return m_displayMode;
+}
+
+
+bool CRelativeUnitSelectorComp::SetSelectedOptionIndex(int index)
+{
+	if (index < 0){
+		return false;
+	}
+
+	if (index > DM_LAST_MODE){
+		return false;
+	}
+
+	if (index != m_displayMode)
+	{
+		ChangeSet changes(iprm::ISelectionParam::CF_SELECTION_CHANGED);
+		istd::CChangeNotifier changeNotfiier(this, &changes);
+
+		m_displayMode = DisplayMode(index);
+	}
+
+	return true;
+}
+
+
+iprm::ISelectionParam* CRelativeUnitSelectorComp::GetSubselection(int /*index*/) const
+{
+	return NULL;
+}
+
+
+// reimplemented (iprm::IOptionsList)
+
+int CRelativeUnitSelectorComp::GetOptionsFlags() const
+{
+	return SCF_SUPPORT_UNIQUE_ID;
+}
+
+
+int CRelativeUnitSelectorComp::GetOptionsCount() const
+{
+	return DM_LAST_MODE + 1;
+}
+
+
+QString CRelativeUnitSelectorComp::GetOptionName(int index) const
+{
+	Q_ASSERT(index >= 0);
+	Q_ASSERT(index < CRelativeUnitSelectorComp::GetOptionsCount());
+
+	switch (index){
+		case DM_NONE:
+			return QObject::tr("None");
+		case DM_PERCENT:
+			return "%";
+		case DM_PERMILLE:
+			return QChar(0x2030);
+		default:
+			return QString();
+	}
+}
+
+
+QString CRelativeUnitSelectorComp::GetOptionDescription(int /*index*/) const
+{
+	return QString();
+}
+
+
+QByteArray CRelativeUnitSelectorComp::GetOptionId(int index) const
+{
+	Q_ASSERT(index >= 0);
+	Q_ASSERT(index < CRelativeUnitSelectorComp::GetOptionsCount());
+
+	switch (index){
+		case DM_NONE:
+			return "none";
+		case DM_PERCENT:
+			return "percent";
+		case DM_PERMILLE:
+			return "permille";
+		default:
+			return QByteArray();
+	}
+}
+
+
+bool CRelativeUnitSelectorComp::IsOptionEnabled(int /*index*/) const
+{
+	return true;
+}
+
+
+// reimplemented (imath::IUnitInfo)
+
+int CRelativeUnitSelectorComp::GetUnitType() const
+{
+	return imath::IUnitInfo::UT_RELATIVE;
+}
+
+
+QString CRelativeUnitSelectorComp::GetUnitName() const
+{
+	switch (m_displayMode){
+		case DM_PERCENT:
+			return "%";
+		case DM_PERMILLE:
+			return QChar(2030);
+		default:
+			return QString();
+	}
+}
+
+
+double CRelativeUnitSelectorComp::GetDisplayMultiplicationFactor() const
+{
+	switch (m_displayMode){
+		case DM_PERCENT:
+			return 100.0;
+		case DM_PERMILLE:
+			return 10.0;
+	}
+
+	return 1.0;
+}
+
+
+istd::CRange CRelativeUnitSelectorComp::GetValueRange() const
+{
+	return istd::CRange(0, 1.0);
+}
+
+
+const imath::IDoubleManip& CRelativeUnitSelectorComp::GetValueManip() const
+{
+	return m_doubleManip;
+}
+
+
+// reimplemented (imath::IDoubleManip)
+
+int CRelativeUnitSelectorComp::GetPrecision() const
+{
+	return m_precision;
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CRelativeUnitSelectorComp::Serialize(iser::IArchive& archive)
+{
+	bool retVal = true;
+
+	istd::CChangeNotifier notifier(archive.IsStoring()? NULL: this);
+
+	static iser::CArchiveTag displayModeTag("DisplayMode", "Display mode", iser::CArchiveTag::TT_LEAF);
+	retVal = retVal && archive.BeginTag(displayModeTag);
+	retVal = I_SERIALIZE_ENUM(DisplayMode, archive, m_displayMode);
+	retVal = retVal && archive.EndTag(displayModeTag);
+
+	return retVal;
+}
+
+
+// protected methods
+
+// reimplemented (ibase::CComponentBase)
+
+void CRelativeUnitSelectorComp::OnComponentCreated()
+{
+	BaseClass::OnComponentCreated();
+
+	m_precision = qMax(0, *m_precisionAttrPtr);
+}
+
+
+} // namespace imeas
+
+
