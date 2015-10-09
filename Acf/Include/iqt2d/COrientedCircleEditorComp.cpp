288a6a68c75479db6d39e15d69f6483fd67ec2a0
am <am@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-10-09 11:33:46 +0000

    COrientedCircleEditorComp change added
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1674 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iqt2d/COrientedCircleEditorComp.cpp
+++ Acf/Include/iqt2d/COrientedCircleEditorComp.cpp
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org or write info@imagingtools.de for further
-** 	information about the ACF.
-**
-********************************************************************************/
-
-
 #include "iqt2d/COrientedCircleEditorComp.h"
 
 
@@ -38,7 +16,7 @@ COrientedCircleEditorComp::COrientedCircleEditorComp()
 
 // reimplemented (iqt2d::TCircleBasedParamsGuiComp)
 
-bool COrientedCircleEditorComp::PopulateActions(QWidget& host, imod::IModel* modelPtr)
+bool COrientedCircleEditorComp::PopulateActions(IActionAdapter& host, imod::IModel* modelPtr)
 {
 	if (!BaseClass::PopulateActions(host, modelPtr)){
 		return false;
@@ -46,7 +24,7 @@ bool COrientedCircleEditorComp::PopulateActions(QWidget& host, imod::IModel* mod
 
 	i2d::COrientedCircle* circlePtr = dynamic_cast<i2d::COrientedCircle*>(modelPtr);
 	if (circlePtr != NULL){
-		host.addAction(&m_reversePolarityAction);
+		host.AddAction(m_reversePolarityAction);
 	}
 
 	return true;
