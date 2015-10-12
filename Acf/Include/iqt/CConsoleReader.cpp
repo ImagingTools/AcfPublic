773c1d2d48fab8b6328727b716d88b612ef25c83
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-10-12 11:44:55 +0000

    Pending changes.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1679 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iqt/CConsoleReader.cpp
+++ Acf/Include/iqt/CConsoleReader.cpp
@@ -1,31 +1,33 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org or write info@imagingtools.de for further
-** 	information about the ACF.
-**
-********************************************************************************/
-
-
 #include <iqt/CConsoleReader.h>
 
 
+#ifdef Q_OS_WIN
 // STD includes
 #include <stdio.h>
 #include <conio.h>
+#else
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+extern "C" void kbhit()
+{
+	static struct termios oldt, newt;
+	tcgetattr( STDIN_FILENO, &oldt);
+	newt = oldt;
+
+	newt.c_lflag &= ~(ICANON | ECHO);
+
+	tcsetattr( STDIN_FILENO, TCSANOW, &newt);
+
+	int retVal = getchar();
+
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+
+	return retVal;
+}
+#endif
+
 
 namespace iqt
 {
@@ -84,6 +86,8 @@ void CConsoleReader::InputObserver::run()
 	while (!m_shouldBeFinished){
 		if (kbhit() != 0){
 			emit m_parent.KeyPressedSignal(getch());
+
+			msleep(100);
 		}
 	}
 }
