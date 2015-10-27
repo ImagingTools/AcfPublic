30e0aea2130789ad23d63184fde2763ec9be41a4
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-10-27 10:24:18 +0000

    Automatic update from Build Server ACF version 2.0.0.7 (3997) ACF-Solutions version 2.0.0.5 (1470) IACF version 0.4.0.82 (713)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1715 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iser/CBitMemoryWriteArchive.cpp
+++ Acf/Include/iser/CBitMemoryWriteArchive.cpp
@@ -34,12 +34,12 @@ CBitMemoryWriteArchive::CBitMemoryWriteArchive(const IVersionInfo* versionInfoPt
 {
 	m_bitPosition = 0;
 
-    int oldSize = m_dataBuffer.size();
+	int oldSize = m_dataBuffer.size();
 
-    if (initialBufferSize > oldSize){
-        m_dataBuffer.resize(initialBufferSize);
-        memset(m_dataBuffer.data() + oldSize, 0, initialBufferSize - oldSize);
-    }
+	if (initialBufferSize > oldSize){
+		m_dataBuffer.resize(initialBufferSize);
+		memset(m_dataBuffer.data() + oldSize, 0, initialBufferSize - oldSize);
+	}
 
 }
 
@@ -52,39 +52,39 @@ void CBitMemoryWriteArchive::WriteValue(quint32 inputValue, int bitsCount)
 	int newBitPosition = m_bitPosition + bitsCount;
 	int newSize = newBitPosition / 8 + ((newBitPosition % 8) != 0);
 	int oldSize = m_dataBuffer.size();
-    
-    if(newSize > oldSize){
-        m_dataBuffer.resize(newSize);
-        memset(m_dataBuffer.data() + oldSize, 0, newSize - oldSize);
-    }
 
-    if (m_bitPosition % 8 == 0 && bitsCount % 8 == 0){
-        int byteCount = bitsCount / 8;
-        int bytePosition = m_bitPosition / 8;
+	if(newSize > oldSize){
+		m_dataBuffer.resize(newSize);
+		memset(m_dataBuffer.data() + oldSize, 0, newSize - oldSize);
+	}
 
-        for (int i = 0; i < byteCount; i++){
-            memset(m_dataBuffer.data() + bytePosition + i, ((unsigned char *)(&inputValue))[byteCount - (i + 1)], 1);
-        }
-    }
-    else {
-        for (int bitIndex = 0; bitIndex < bitsCount; bitIndex++){
-            int localByteIndex = (m_bitPosition + bitIndex) / 8;
-            int contextBitIndex = bitIndex % bitsCount;
-            int globalBitIndex = (m_bitPosition + bitIndex) % 8;
-            int contextMask = (1 << (bitsCount - contextBitIndex - 1));
+	if (m_bitPosition % 8 == 0 && bitsCount % 8 == 0){
+		int byteCount = bitsCount / 8;
+		int bytePosition = m_bitPosition / 8;
 
-            bool bitOn = (inputValue & contextMask) != 0;
+		for (int i = 0; i < byteCount; i++){
+			memset(m_dataBuffer.data() + bytePosition + i, ((unsigned char *)(&inputValue))[byteCount - (i + 1)], 1);
+		}
+	}
+	else {
+		for (int bitIndex = 0; bitIndex < bitsCount; bitIndex++){
+			int localByteIndex = (m_bitPosition + bitIndex) / 8;
+			int contextBitIndex = bitIndex % bitsCount;
+			int globalBitIndex = (m_bitPosition + bitIndex) % 8;
+			int contextMask = (1 << (bitsCount - contextBitIndex - 1));
 
-            unsigned char& bufferValue = (unsigned char&)(*(m_dataBuffer.data() + localByteIndex));
-    		
-            if (bitOn){
-	    		bufferValue |= 1 << (8 - globalBitIndex - 1);
-		    }
-		    else{
-			    bufferValue &= ~(1 << (8 - globalBitIndex - 1));
-		    }
-	    }
-    }
+			bool bitOn = (inputValue & contextMask) != 0;
+
+			unsigned char& bufferValue = (unsigned char&)(*(m_dataBuffer.data() + localByteIndex));
+
+			if (bitOn){
+				bufferValue |= 1 << (8 - globalBitIndex - 1);
+			}
+			else{
+				bufferValue &= ~(1 << (8 - globalBitIndex - 1));
+			}
+		}
+	}
 
 	m_bitPosition += bitsCount;
 }
