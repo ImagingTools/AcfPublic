866fabde7b87e2887e210c399c97e3c5b39b0a2b
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-11-03 23:24:48 +0000

    Automatic update from Build Server ACF version 2.0.0.25 (4015) ACF-Solutions version 2.0.0.15 (1480) IACF version 0.4.0.88 (719)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1736 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Acf/Include/iwidgets/CFocusDecorator.cpp
@@ -0,0 +1,129 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#include "iwidgets/CFocusDecorator.h"
+
+
+namespace iwidgets
+{
+
+
+// public methods
+
+CFocusDecorator::CFocusDecorator(QObject* parentPtr)
+	:QObject(parentPtr)
+{
+}
+
+CFocusDecorator::~CFocusDecorator()
+{
+	UnregisterAllWidgets();
+}
+
+
+bool CFocusDecorator::RegisterWidget(QWidget* widgetPtr, QGraphicsEffect* effectPtr)
+{
+	if ((widgetPtr != NULL) || (effectPtr != NULL)){
+		return false;
+	}
+
+	if (m_widgetEffectsMap.contains(widgetPtr)){
+		return true;
+	}
+
+	widgetPtr->installEventFilter(this);
+
+	widgetPtr->setGraphicsEffect(effectPtr);
+
+	m_widgetEffectsMap[widgetPtr] = effectPtr;
+
+	return true;
+}
+
+
+void CFocusDecorator::UnregisterWidget(QWidget* widgetPtr)
+{
+	if (widgetPtr == NULL){
+		return;
+	}
+
+	Q_ASSERT(m_widgetEffectsMap.contains(widgetPtr));
+	if (m_widgetEffectsMap.contains(widgetPtr)){
+		widgetPtr->removeEventFilter(this);
+	}
+
+	m_widgetEffectsMap.remove(widgetPtr);
+
+	widgetPtr->setGraphicsEffect(NULL);
+}
+
+
+void CFocusDecorator::UnregisterAllWidgets()
+{
+	while(!m_widgetEffectsMap.isEmpty()){
+		QWidget* widgetPtr = m_widgetEffectsMap.begin().key();
+
+		UnregisterWidget(widgetPtr);
+	}
+}
+
+
+// protected methods
+
+// reimplemented (QObject)
+
+bool CFocusDecorator::eventFilter(QObject* objectPtr, QEvent* eventPtr)
+{
+	if (objectPtr != NULL && eventPtr != NULL){
+		int eventType = eventPtr->type();
+		switch (eventType){
+			case QEvent::FocusIn:{
+				QWidget* widgetPtr = dynamic_cast<QWidget*>(objectPtr);
+				if (widgetPtr != NULL){
+					QGraphicsEffect* effectPtr = m_widgetEffectsMap.value(widgetPtr, NULL);
+					if (effectPtr != NULL){
+						widgetPtr->setGraphicsEffect(effectPtr);
+					}
+				}
+				break;
+			}
+
+			case QEvent::FocusOut:{
+				QWidget* widgetPtr = dynamic_cast<QWidget*>(objectPtr);
+				if (widgetPtr != NULL){
+					QGraphicsEffect* effectPtr = m_widgetEffectsMap.value(widgetPtr, NULL);
+					if (effectPtr != NULL){
+						effectPtr->setParent(this);
+
+						widgetPtr->setGraphicsEffect(NULL);
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	return QObject::eventFilter(objectPtr, eventPtr);
+}
+
+
+} // namespace iwidgets
---
+++ Acf/Include/iwidgets/CFocusDecorator.h
@@ -0,0 +1,68 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#pragma once
+
+
+// Qt includes
+#include <QtCore/QtGlobal>
+#include <QtCore/QMap>
+#include <QtCore/QEvent>
+#if QT_VERSION >= 0x050000
+#include <QtWidgets/QGraphicsEffect>
+#include <QtWidgets/QWidget>
+#else
+#include <QtGui/QGraphicsEffect>
+#include <QtGui/QWidget>
+#endif
+
+
+namespace iwidgets
+{
+
+
+/*
+	Helper class for creating widget decorations based on graphics effects, such drop shadow.
+*/
+class CFocusDecorator: public QObject
+{
+public:
+	CFocusDecorator(QObject* parentPtr = NULL);
+	virtual ~CFocusDecorator();
+
+	bool RegisterWidget(QWidget* widgetPtr, QGraphicsEffect* effectPtr);
+	void UnregisterWidget(QWidget* widgetPtr);
+	void UnregisterAllWidgets();
+
+protected:
+	// reimplemented (QObject)
+	virtual bool eventFilter(QObject* objectPtr, QEvent* eventPtr);
+
+private:
+	typedef QMap<QWidget*, QGraphicsEffect*> WidgetEffectsMap;
+	WidgetEffectsMap m_widgetEffectsMap;
+};
+
+
+} // namespace iwidgets
+
+
