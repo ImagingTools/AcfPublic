0e0a98ca18e7d83b4bb5d5c93ab5aa47be2a9c7a
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2015-12-19 10:34:09 +0000

    Automatic update from Build Server ACF version 2.0.0.69 (4059) ACF-Solutions version 2.0.0.46 (1511) IACF version 0.4.0.96 (727)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1772 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Impl/HotfolderPck/HotfolderPck.h
+++ AcfSln/Impl/HotfolderPck/HotfolderPck.h
@@ -27,10 +27,7 @@
 #include "icomp/TModelCompWrap.h"
 #include "icomp/TMakeComponentWrap.h"
 
-#include "ihotf/CDirectoryMonitorParams.h"
-#include "ihotf/CHotfolderProcessingInfo.h"
 #include "ihotf/CMonitoringSessionsManager.h"
-#include "ihotf/CHotfolderStatistics.h"
 #include "ihotf/CDirectoryMonitorParamsComp.h"
 #include "ihotf/CFileSystemChangeStorage.h"
 
@@ -44,22 +41,10 @@ namespace HotfolderPck
 
 typedef icomp::TModelCompWrap<
 			icomp::TMakeComponentWrap<
-						ihotf::CDirectoryMonitorParams, iser::ISerializable, ihotf::IDirectoryMonitorParams> > DirectoryMonitorParams;
-
-typedef icomp::TModelCompWrap<
-			icomp::TMakeComponentWrap<
-						ihotf::CHotfolderProcessingInfo, iser::ISerializable, ihotf::IHotfolderProcessingInfo> > HotfolderProcessingInfo;
-
-typedef icomp::TModelCompWrap<
-			icomp::TMakeComponentWrap<
 						ihotf::CMonitoringSessionsManager, iser::ISerializable, ihotf::IMonitoringSessionManager> > MonitoringSessionsManager;
 
 typedef icomp::TModelCompWrap<
-			icomp::TMakeComponentWrap<
-						ihotf::CHotfolderStatistics, iser::ISerializable, imod::IObserver, ihotf::IHotfolderStatistics> > HotfolderStatistics;
-
-typedef icomp::TModelCompWrap<
-			ihotf::CDirectoryMonitorParamsComp> AttributedDirectoryMonitorParams;
+			ihotf::CDirectoryMonitorParamsComp> DirectoryMonitorParams;
 
 typedef icomp::TModelCompWrap<
 			icomp::TMakeComponentWrap<
