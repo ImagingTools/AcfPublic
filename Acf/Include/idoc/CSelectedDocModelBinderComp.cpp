fe8f615581e02d46cc6ff9219bbcf5a65b0d6f4f
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-01-16 01:17:28 +0000

    Automatic update from Build Server ACF version 2.0.0.84 (4074) ACF-Solutions version 2.0.0.53 (1518) IACF version 0.4.0.96 (727)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1796 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/idoc/CSelectedDocModelBinderComp.cpp
+++ Acf/Include/idoc/CSelectedDocModelBinderComp.cpp
@@ -23,6 +23,10 @@
 #include "idoc/CSelectedDocModelBinderComp.h"
 
 
+// Qt includes
+#include <QtCore/QFileInfo>
+
+
 namespace idoc
 {
 
@@ -63,7 +67,20 @@ void CSelectedDocModelBinderComp::TryConnectObservers()
 	}
 
 	const icomp::IComponent* viewCompPtr = dynamic_cast<const icomp::IComponent*>(viewPtr);
-	const icomp::IComponent* documentCompPtr = dynamic_cast<const icomp::IComponent*>(m_documentManagerCompPtr->GetDocumentFromView(*viewPtr));
+	IDocumentManager::DocumentInfo documentInfo;
+	const icomp::IComponent* documentCompPtr = dynamic_cast<const icomp::IComponent*>(m_documentManagerCompPtr->GetDocumentFromView(*viewPtr, &documentInfo));
+
+	if (documentInfo.filePath != m_selectedDocumentInfo.filePath){
+		istd::CChangeNotifier infoNotifier(&m_selectedDocumentInfo);
+		Q_UNUSED(infoNotifier);
+
+		m_selectedDocumentInfo.filePath = documentInfo.filePath;
+
+		QFileInfo fileInfo(documentInfo.filePath);
+
+		m_selectedDocumentInfo.filePath = fileInfo.fileName();
+	}
+
 	m_isActive =
 				((viewCompPtr != NULL) && (observedObjectPtr == viewCompPtr)) ||
 				((documentCompPtr != NULL) && (observedObjectPtr == documentCompPtr));
@@ -145,6 +162,54 @@ void CSelectedDocModelBinderComp::OnComponentDestroyed()
 	}
 
 	BaseClass::OnComponentDestroyed();
+}
+
+
+// public methods of embedded class DocumentInfo
+
+// reimplemented (iprm::INameParam)
+
+const QString& CSelectedDocModelBinderComp::DocumentInfo::GetName() const
+{
+	return name;
+}
+
+
+void CSelectedDocModelBinderComp::DocumentInfo::SetName(const QString& /*name*/)
+{
+}
+
+
+bool CSelectedDocModelBinderComp::DocumentInfo::IsNameFixed() const
+{
+	return true;
+}
+
+
+// reimplemented (ifile::IFileNameParam)
+
+int CSelectedDocModelBinderComp::DocumentInfo::GetPathType() const
+{
+	return PT_FILE;
+}
+
+
+const QString& CSelectedDocModelBinderComp::DocumentInfo::GetPath() const
+{
+	return filePath;
+}
+
+
+void CSelectedDocModelBinderComp::DocumentInfo::SetPath(const QString& /*path*/)
+{
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CSelectedDocModelBinderComp::DocumentInfo::Serialize(iser::IArchive& /*archive*/)
+{
+	return false;
 }
 
 
--- Acf/Include/idoc/CSelectedDocModelBinderComp.h
+++ Acf/Include/idoc/CSelectedDocModelBinderComp.h
@@ -26,7 +26,10 @@
 
 // ACF includes
 #include "imod/TSingleModelObserverBase.h"
+#include "imod/TModelWrap.h"
 #include "icomp/CComponentBase.h"
+#include "iprm/INameParam.h"
+#include "ifile/IFileNameParam.h"
 #include "idoc/IDocumentManager.h"
 
 
@@ -49,6 +52,11 @@ public:
 	typedef imod::TSingleModelObserverBase<IDocumentManager> BaseClass2;
 
 	I_BEGIN_COMPONENT(CSelectedDocModelBinderComp);
+		I_REGISTER_SUBELEMENT(SelectedDocumentInfo);
+		I_REGISTER_SUBELEMENT_INTERFACE(SelectedDocumentInfo, iprm::INameParam, ExtractSelectedDocumentInfo);
+		I_REGISTER_SUBELEMENT_INTERFACE(SelectedDocumentInfo, ifile::IFileNameParam, ExtractSelectedDocumentInfo);
+		I_REGISTER_SUBELEMENT_INTERFACE(SelectedDocumentInfo, imod::IModel, ExtractSelectedDocumentInfo);
+		I_REGISTER_SUBELEMENT_INTERFACE(SelectedDocumentInfo, istd::IChangeable, ExtractSelectedDocumentInfo);
 		I_ASSIGN(m_observedObjectCompPtr, "ActivationObject", "Object will be observed if it is active", true, "ActivationObject");
 		I_ASSIGN(m_documentManagerCompPtr, "DocumentManager", "Document manager used to check which object is active", true, "DocumentManager");
 		I_ASSIGN_TO(m_documentManagerModelCompPtr, m_documentManagerCompPtr, true);
@@ -77,10 +85,41 @@ private:
 	I_REF(imod::IModel, m_modelCompPtr);
 	I_MULTIREF(imod::IObserver, m_observersCompPtr);
 
+	// static template methods for subelement access
+	template <class InterfaceType>
+	static InterfaceType* ExtractSelectedDocumentInfo(CSelectedDocModelBinderComp& component)
+	{
+		return &component.m_selectedDocumentInfo;
+	}
+
+	class DocumentInfo:
+				virtual public iprm::INameParam,
+				virtual public ifile::IFileNameParam
+	{
+	public:
+		// reimplemented (iprm::INameParam)
+		virtual const QString& GetName() const;
+		virtual void SetName(const QString& name);
+		virtual bool IsNameFixed() const;
+
+		// reimplemented (ifile::IFileNameParam)
+		virtual int GetPathType() const;
+		virtual const QString& GetPath() const;
+		virtual void SetPath(const QString& path);
+
+		// reimplemented (iser::ISerializable)
+		virtual bool Serialize(iser::IArchive& archive);
+
+		QString name;
+		QString filePath;
+	};
+
 	bool m_isActive;
 
 	typedef QMap<imod::IObserver*, imod::IModel*> ModelObserverMap;
 	ModelObserverMap m_connectedMap;
+
+	imod::TModelWrap<DocumentInfo> m_selectedDocumentInfo;
 };
 
 
