67046fb35322ed8c9480a14b6d420a27939f7dd1
buildserver <buildserver@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-03-24 11:52:17 +0000

    Fixed shutdown bug, set timeout to 60 secs.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1871 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/iservice/CServiceApplicationComp.cpp
+++ AcfSln/Include/iservice/CServiceApplicationComp.cpp
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF-Solutions Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org or write info@imagingtools.de for further
-** 	information about the ACF.
-**
-********************************************************************************/
-
-
 #include "iservice/CServiceApplicationComp.h"
 
 
@@ -252,7 +230,14 @@ CServiceApplicationComp::CService::CService(
 	m_parent(parent),
 	m_application(application)
 {
-	m_applicationArguments = m_parent.GetApplicationArguments(serviceArgc, serviceArgv);
+    QStringList applicationArguments;
+
+    applicationArguments = m_parent.GetApplicationArguments(serviceArgc, serviceArgv);
+
+    for (QString argument : applicationArguments)
+    {
+        m_applicationArguments.push_back(argument.toUtf8());
+    }
 
 	setServiceFlags(QtServiceBase::CanBeSuspended);
 }
@@ -287,7 +272,6 @@ void CServiceApplicationComp::CService::start()
 
 void CServiceApplicationComp::CService::stop()
 {
-	qApp->exit();
 }
 
 
@@ -319,6 +303,16 @@ void CServiceApplicationComp::CService::createApplication(int&/*argc*/, char** /
 {
 	QVector<char*> argv = GetApplicationArguments();
 
+	int argCount = argv.count();
+
+	iqtgui::IGuiApplication* guiAppPtr = dynamic_cast<iqtgui::IGuiApplication*>(&m_application);
+	if (guiAppPtr != NULL){
+		new QApplication(argCount, argv.data());	
+	}
+	else{
+		new QCoreApplication(argCount, argv.data());
+	}
+
 	m_application.InitializeApplication(argv.count(), argv.data());
 }
 
@@ -327,7 +321,7 @@ int CServiceApplicationComp::CService::executeApplication()
 {
 	QVector<char*> argv = GetApplicationArguments();
 
-	return m_application.Execute(argv.count(), argv.data());
+    return m_application.Execute(argv.count(), argv.data());
 }
 
 
@@ -335,13 +329,13 @@ int CServiceApplicationComp::CService::executeApplication()
 
 QVector<char*> CServiceApplicationComp::CService::GetApplicationArguments() const
 {
-	QVector<char*> argv(m_applicationArguments.count());
+    QVector<char*> argv(m_applicationArguments.count());
 
-	for (int argIndex = 0; argIndex < m_applicationArguments.count(); argIndex++){
-		argv[argIndex] = m_applicationArguments[argIndex].toLocal8Bit().data();
-	}
+    for (int argIndex = 0; argIndex < m_applicationArguments.count(); argIndex++){
+        argv[argIndex] = (char*)m_applicationArguments[argIndex].data();
+    }
 
-	return argv;
+    return argv;
 }
 
 #endif // Q_OS_WIN
--- AcfSln/Include/iservice/CServiceApplicationComp.h
+++ AcfSln/Include/iservice/CServiceApplicationComp.h
@@ -26,6 +26,8 @@
 
 // Qt includes
 #include <QtGui/QIcon>
+#include <QtCore/QList>
+#include <QtCore/QByteArray>
 #if QT_VERSION >= 0x050000
 #include <QtWidgets/QSystemTrayIcon>
 #else
@@ -111,7 +113,7 @@ protected:
 	private:
 		CServiceApplicationComp& m_parent;
 		ibase::IApplication& m_application;
-		QStringList m_applicationArguments;
+        QList<QByteArray>  m_applicationArguments;
 	};
 
 	istd::TDelPtr<CService> m_servicePtr;
