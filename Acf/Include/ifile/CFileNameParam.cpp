576dbc28a182643ef97be4bc30cdc77641644493
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-06-10 11:11:01 +0000

    Pending changes
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1929 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Acf/Include/ifile/CFileNameParam.cpp
@@ -0,0 +1,100 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#include "ifile/CFileNameParam.h"
+
+
+// Qt includes
+#include <QtCore/QFileInfo>
+
+// ACF includes
+#include "istd/CChangeNotifier.h"
+#include "iser/IArchive.h"
+#include "iser/CArchiveTag.h"
+
+
+namespace ifile
+{
+
+
+// reimplemented (ifile::IFileNameParam)
+
+int CFileNameParam::GetPathType() const
+{
+	int pathType = IFileNameParam::PT_UNKNOWN;
+
+	QFileInfo info(m_path);
+
+	if (info.isDir()){
+		pathType = IFileNameParam::PT_DIRECTORY;
+	} 
+	else if (info.isFile()){
+		pathType = IFileNameParam::PT_FILE;
+	}
+
+	return pathType;
+}
+
+
+const QString& CFileNameParam::GetPath() const
+{
+	return m_path;
+}
+
+
+void CFileNameParam::SetPath(const QString& path)
+{
+	if (path != m_path){
+		istd::CChangeNotifier notifier(this);
+
+		m_path = path;
+	}
+}
+
+
+// reimplemented (iser::ISerializable)
+
+bool CFileNameParam::Serialize(iser::IArchive& archive)
+{
+	bool retVal = true;
+
+	QString filePath = m_path;
+
+	static iser::CArchiveTag pathTag("Path", "File path", iser::CArchiveTag::TT_LEAF);
+	retVal = retVal && archive.BeginTag(pathTag);
+	retVal = retVal && archive.Process(filePath);
+	retVal = retVal && archive.EndTag(pathTag);
+
+	if (!archive.IsStoring() && (filePath != m_path)){
+		istd::CChangeNotifier notifier(this, &GetAllChanges());
+		Q_UNUSED(notifier);
+
+		m_path = filePath;
+	}
+
+	return retVal;
+}
+
+
+} // namespace ifile
+
+
---
+++ Acf/Include/ifile/CFileNameParam.h
@@ -0,0 +1,62 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#ifndef ifile_CFileNameParam_included
+#define ifile_CFileNameParam_included
+
+
+// ACF includes
+#include "iser/ISerializable.h"
+#include "icomp/CComponentBase.h"
+#include "ifile/IFileNameParam.h"
+
+
+namespace ifile
+{
+
+
+/**
+	Basic implementation of interface ifile::IFileNameParam.
+*/
+class CFileNameParam: virtual public IFileNameParam
+{
+public:
+
+	// reimplemented (ifile::IFileNameParam)
+	virtual int GetPathType() const;
+	virtual const QString& GetPath() const;
+	virtual void SetPath(const QString& path);
+
+	// reimplemented (iser::ISerializable)
+	virtual bool Serialize(iser::IArchive& archive);
+
+private:
+	QString m_path;
+};
+
+
+} // namespace ifile
+
+
+#endif // !ifile_CFileNameParam_included
+
+
