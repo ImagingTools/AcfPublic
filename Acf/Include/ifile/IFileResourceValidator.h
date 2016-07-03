46061b0795fcb16fe5d4bd94bd7e648cf251d4c8
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-07-03 14:38:00 +0000

    Automatic update from Build Server ACF version 2.0.1.64 (4209) ACF-Solutions version 2.1.0.8 (1590) IACF version 0.4.0.102 (733)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1950 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Acf/Include/ifile/IFileResourceValidator.h
@@ -0,0 +1,55 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#pragma once
+
+
+// ACF includes
+#include <istd/IInformationProvider.h>
+#include <ifile/IFileResourcesManager.h>
+
+
+namespace ifile
+{
+
+
+/**
+	Interface for validation of the file-based resources.
+*/
+class IFileResourceValidator: virtual public istd::IPolymorphic
+{
+public:
+	/**
+		Do validation of the given resource.
+		\param resourceId			ID of the resource in the repository.
+		\param messageConsumerPtr	Consumer of operation messages (optionally).
+	*/
+	virtual istd::IInformationProvider::InformationCategory ValidateResource(
+				const QByteArray& resourceId,
+				const ifile::IFileResourcesManager* fileRepositoryPtr,
+				ilog::IMessageConsumer* messageConsumerPtr = NULL) const = 0;
+};
+
+
+} // namespace ifile
+
+
