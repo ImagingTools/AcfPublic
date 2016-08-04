f79fb1338b4e69dabcf76a2f9f7b31d401be5ca8
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-08-04 09:19:07 +0000

    Try to fix Mac build
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1983 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/i2d/CLine2d.h
+++ Acf/Include/i2d/CLine2d.h
@@ -1,25 +1,3 @@
-/********************************************************************************
-**
-**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
-**
-**	This file is part of the ACF Toolkit.
-**
-**	This file may be used under the terms of the GNU Lesser
-**	General Public License version 2.1 as published by the Free Software
-**	Foundation and appearing in the file LicenseLGPL.txt included in the
-**	packaging of this file.  Please review the following information to
-**	ensure the GNU Lesser General Public License version 2.1 requirements
-**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-**	If you are unsure which license is appropriate for your use, please
-**	contact us at info@imagingtools.de.
-**
-** 	See http://www.ilena.org or write info@imagingtools.de for further
-** 	information about the ACF.
-**
-********************************************************************************/
-
-
 #ifndef i2d_CLine2d_included
 #define i2d_CLine2d_included
 
@@ -431,15 +409,15 @@ inline void CLine2d::SetPoint2Quiet(const CVector2d& point)
 }
 
 
-} // namespace i2d
-
-
 // related global functions
 
 inline uint qHash(const i2d::CLine2d& key, uint seed = 0)
 {
 	return ::qHash(key.GetPoint1(), seed) ^ ::qHash(key.GetPoint2(), seed);
 }
+
+
+} // namespace i2d
 
 
 #endif // !i2d_CLine2d_included
