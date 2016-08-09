bf5c52ff3ce399433cef51aa1c681155008dc29f
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-08-09 16:34:57 +0000

    VC9 generation scripts corrected.
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@1990 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Build/VC9/_CreateVCProjectsFromQMake.bat
+++ Build/VC9/_CreateVCProjectsFromQMake.bat
@@ -1,5 +1,6 @@
 @echo off
 
+set QTDIR=%QTDIR_32%
 set COMPILER_EXT=VC9
 set QMAKESPEC=%QTDIR%\mkspecs\win32-msvc2008
 set path=%path%;%QTDIR%\bin
@@ -12,7 +13,7 @@ cd Build\QMake
 %QTDIR%\bin\qmake -recursive -tp vc
 cd %~dp0\..\..
 
-call Acf\Config\QMake\CopyVCProjToSubdir.js %COMPILER_EXT%
+call Acf\Config\QMake\CopyVCProjToSubdir.js %COMPILER_EXT% -replace%QTDIR%=$(QTDIR) -replace%ACF_TOOLS_BIN%\=$(ACF_TOOLS_BIN)\ -replace%CD%\=$(RelativePath)\ -qt_verWin32=QT_VC9_32
 
 
 cd %~dp0\
