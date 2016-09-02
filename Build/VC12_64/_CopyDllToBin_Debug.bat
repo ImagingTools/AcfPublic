8933c1bace7e738f82cd9f2203d6782f253d3847
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-09-02 11:42:49 +0000

    Deployment scripts added
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2021 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Build/VC12_64/_CopyDllToBin_Debug.bat
@@ -0,0 +1,5 @@
+%QTDIR_VC12_64%bin\windeployqt --force ..\..\Acf\Bin\DebugVC12_64\Acf.exe
+%QTDIR_VC12_64%bin\windeployqt --force ..\..\AcfSln\Bin\DebugVC12_64\Compositor.exe
+
+
+pause
---
+++ Build/VC12_64/_CopyDllToBin_Release.bat
@@ -0,0 +1,4 @@
+%QTDIR_VC12_64%bin\windeployqt --force -release ..\..\Acf\Bin\ReleaseVC12_64\Acf.exe
+%QTDIR_VC12_64%bin\windeployqt --force -release ..\..\AcfSln\Bin\ReleaseVC12_64\Compositor.exe
+
+pause
