3fa4f63fe748d11c81b8f4f65c77232734027552
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-09-12 16:04:38 +0000

    Automatic update from Build Server ACF version 2.0.1.144 (4289) ACF-Solutions version 2.1.0.68 (1650) IACF version 0.4.0.119 (750)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2029 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iwidgets/iwidgets.cpp
+++ Acf/Include/iwidgets/iwidgets.cpp
@@ -20,33 +20,45 @@
 ********************************************************************************/
 
 
-#ifndef iwidgets_included
-#define iwidgets_included
+#include <iwidgets/iwidgets.h>
 
 
-// ACF includes
-#include "iqt/iqt.h"
+// Qt includes
+#include <QtCore/QtGlobal>
+#if QT_VERSION >= 0x050000
+	#include <QtWidgets/QWidget>
+	#include <QtWidgets/QLayoutItem>
+	#include <QtWidgets/QLayout>
+#else
+	#include <QtGui/QWidget>
+	#include <QtGui/QLayoutItem>
+	#include <QtGui/QLayout>
+#endif
 
 
-class QLayout;
-
-
-/**
-	Namespace contains implementation of Qt widget extensions
-*/
 namespace iwidgets
 {
 
 
-/**
-	Remove all items (widgets and sub-layouts) from a given layout.
-*/
-void ClearLayout(QLayout* layout);
+void ClearLayout(QLayout* layout)
+{
+	while (QLayoutItem* item = layout->takeAt(0))
+	{
+		QWidget* widgetPtr = item->widget();
+		if (widgetPtr != NULL) {
+			delete widgetPtr;
+		}
+		
+		QLayout* childLayoutPtr = item->layout();
+		if (childLayoutPtr != NULL){
+			ClearLayout(childLayoutPtr);
+		}
+
+		delete item;
+	}
+}
 
 
 } // namespace iwidgets
-
-
-#endif // !iwidgets_included
 
 
