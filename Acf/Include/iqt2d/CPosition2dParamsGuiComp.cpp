19504bc88b33c99031e58039bab2bfac2156e77f
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-11-01 18:52:12 +0000

    Automatic update from Build Server ACF version 2.0.1.179 (4324) ACF-Solutions version 2.1.0.90 (1672) IACF version 0.4.0.123 (754)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2075 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iqt2d/CPosition2dParamsGuiComp.cpp
+++ Acf/Include/iqt2d/CPosition2dParamsGuiComp.cpp
@@ -75,6 +75,9 @@ void CPosition2dParamsGuiComp::OnGuiCreated()
 {
 	BaseClass::OnGuiCreated();
 
+	XLabel->setVisible(m_xLabelStrAttrPtr.IsValid());
+	YLabel->setVisible(m_yLabelStrAttrPtr.IsValid());
+
 	if (IsPositionFixed()){
 		XSpin->setEnabled(false);
 		YSpin->setEnabled(false);
@@ -101,7 +104,19 @@ void CPosition2dParamsGuiComp::OnGuiRetranslate()
 {
 	BaseClass::OnGuiRetranslate();
 
-	QString unitName = GetUnitName();
+	if (m_altrnativeStrAttrPtr.IsValid()){
+		PositionLabel->setText(*m_altrnativeStrAttrPtr);
+	}
+
+	if (m_xLabelStrAttrPtr.IsValid()){
+		XLabel->setText(*m_xLabelStrAttrPtr);
+	}
+
+	if (m_yLabelStrAttrPtr.IsValid()){
+		YLabel->setText(*m_yLabelStrAttrPtr);
+	}
+
+	const QString& unitName = GetUnitName();
 
 	if (!unitName.isEmpty()){
 		PositionUnitLabel->setText(unitName);
