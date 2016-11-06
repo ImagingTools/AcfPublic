fb13012a2fe234abedb885b3180e1bfdc9f53bd1
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2016-11-06 00:31:11 +0000

    Automatic update from Build Server ACF version 2.0.1.185 (4330) ACF-Solutions version 2.1.0.91 (1673) IACF version 0.4.0.123 (754)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2081 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Acf/Include/iqtgui/CProcessStartCommandComp.cpp
@@ -0,0 +1,122 @@
+/********************************************************************************
+**
+**	Copyright (C) 2007-2015 Witold Gantzke & Kirill Lepskiy
+**
+**	This file is part of the ACF Toolkit.
+**
+**	This file may be used under the terms of the GNU Lesser
+**	General Public License version 2.1 as published by the Free Software
+**	Foundation and appearing in the file LicenseLGPL.txt included in the
+**	packaging of this file.  Please review the following information to
+**	ensure the GNU Lesser General Public License version 2.1 requirements
+**	will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**	If you are unsure which license is appropriate for your use, please
+**	contact us at info@imagingtools.de.
+**
+** 	See http://www.ilena.org or write info@imagingtools.de for further
+** 	information about the ACF.
+**
+********************************************************************************/
+
+
+#include <iqtgui/CProcessStartCommandComp.h>
+
+
+// Qt includes
+#include <QtCore/QProcess>
+
+
+namespace iqtgui
+{
+
+
+// public methods
+
+CProcessStartCommandComp::CProcessStartCommandComp()
+	:m_fileCommands("&File", 100),
+	m_startProcessCommand("Start...", 100, ibase::ICommand::CF_GLOBAL_MENU, GI_START_PROCESS_COMMAND)
+{
+	connect(&m_startProcessCommand, SIGNAL(triggered()), this, SLOT(OnStartProcessCommand()));
+	m_fileCommands.InsertChild(&m_startProcessCommand);
+
+	m_rootCommands.InsertChild(&m_fileCommands);
+}
+
+
+// reimplemented (ibase::ICommandsProvider)
+
+const ibase::IHierarchicalCommand* CProcessStartCommandComp::GetCommands() const
+{
+	return &m_rootCommands;
+}
+
+
+// protected methods
+
+QStringList CProcessStartCommandComp::GetProcessArguments() const
+{
+	QStringList retVal;
+
+	for (int i = 0; i < m_processArgumentsAttrPtr.GetCount(); ++i){
+		retVal += m_processArgumentsAttrPtr[i];
+	}
+
+	return retVal;
+}
+
+
+bool CProcessStartCommandComp::StartProcess(const QStringList& arguments)
+{
+	Q_ASSERT(m_applicationPathCompPtr.IsValid());
+
+	QString applicationPath = m_applicationPathCompPtr->GetPath();
+	QFileInfo applicationFileInfo(applicationPath);
+
+	if (!applicationFileInfo.exists()){
+		return false;
+	}
+
+#ifdef Q_OS_WIN32
+	if (applicationFileInfo.suffix().isEmpty()){
+		applicationPath += ".exe";
+	}
+#endif
+#ifdef Q_OS_MAC
+	if (!applicationFileInfo.exists() && applicationFileInfo.suffix().isEmpty() && applicationFileInfo.isBundle()){
+		applicationPath += ".app";
+	}
+#endif
+#ifndef QT_NO_PROCESS
+	return QProcess::startDetached(applicationPath, arguments);
+#endif // QT_NO_PROCESS
+
+	return false;
+}
+
+
+// reimplemented (icomp::CComponentBase)
+
+void CProcessStartCommandComp::OnComponentCreated()
+{
+	BaseClass::OnComponentCreated();
+
+	if (!m_applicationPathCompPtr.IsValid()){
+		m_startProcessCommand.setEnabled(false);
+	}
+	
+	m_startProcessCommand.SetName(*m_startProcessCommandNameAttrPtr);
+}
+
+
+// private slots
+
+void CProcessStartCommandComp::OnStartProcessCommand()
+{
+	StartProcess(GetProcessArguments());
+}
+
+
+} // namespace iqtgui
+
+
