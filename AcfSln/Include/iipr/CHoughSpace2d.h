c5b77c0dec4e6844763fda15b2bdd6a0650a9ce2
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2017-03-06 18:00:19 +0000

    Automatic update from Build Server ACF version 2.0.2.6 (4448) ACF-Solutions version 2.1.1.15 (1797) IACF version 0.4.1.2 (769)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2228 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/iipr/CHoughSpace2d.h
+++ AcfSln/Include/iipr/CHoughSpace2d.h
@@ -102,7 +102,7 @@ public:
 	/**
 		Extract this Hough space to some gray scale bitmap.
 	*/
-	bool ExtractToBitmap(iimg::IBitmap& bitmap);
+	bool ExtractToBitmap(iimg::IBitmap& bitmap) const;
 
 	/**
 		Get position in space for some input.
