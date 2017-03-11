4e8be4d1d7ea735e89a132e4309fc929251c76d7
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2017-03-11 01:17:45 +0000

    Automatic update from Build Server ACF version 2.0.2.17 (4459) ACF-Solutions version 2.1.1.21 (1803) IACF version 0.4.1.2 (769)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2241 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/iipr/CDirectionalOptimalPathSolver.h
+++ AcfSln/Include/iipr/CDirectionalOptimalPathSolver.h
@@ -35,7 +35,7 @@
 
 #ifdef __clang__
 	#define __forceinline __attribute__((always_inline))
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) && !defined(__forceinline)
 	#define __forceinline inline
 #endif
 
