f7d6715b8d119aa539a3f521f11b77009ad2649b
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2017-06-29 08:50:26 +0000

    Automatic update from Build Server ACF version 2.0.2.89 (4531) ACF-Solutions version 2.1.1.84 (1866) IACF version 0.4.1.16 (783)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2331 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/ialgo/TNeighbourBinCaller.h
+++ AcfSln/Include/ialgo/TNeighbourBinCaller.h
@@ -20,8 +20,8 @@
 ********************************************************************************/
 
 
-#ifndef ialgo_CNeighbourBinCaller_included
-#define ialgo_CNeighbourBinCaller_included
+#ifndef ialgo_TNeighbourBinCaller_included
+#define ialgo_TNeighbourBinCaller_included
 
 
 // ACF includes
@@ -35,10 +35,11 @@ namespace ialgo
 /**
 	Class providing mechanism to iterate over neighbours in some multidimensional discrete space.
 */
-class CNeighbourBinCaller
+template <int SpaceDimension>
+class TNeighbourBinCaller
 {
 public:
-	CNeighbourBinCaller();
+	TNeighbourBinCaller();
 
 	void InitForCyclicSpace(const istd::CVarIndex& spaceSizes, int leftTolerance, int rightTolerance);
 
@@ -50,15 +51,15 @@ public:
 
 		visitor.BeginVisiting();
 
-		istd::TIndex<CFastModelFinder::FEATURE_LEVELS_COUNT> imageAngleIndices = istd::TIndex<CFastModelFinder::FEATURE_LEVELS_COUNT>::GetZero();
+		istd::TIndex<SpaceDimension> imageAngleIndices = istd::TIndex<SpaceDimension>::GetZero();
 		do{
 			int imageBinIndex = imageAngleIndices.GetIterationIndex(m_spaceSizes);
 
 			if (visitor.BeginImageBin(imageBinIndex)){
 				istd::CVarIndex angleDeltaIndices(m_spaceSizes.GetDimensionsCount(), 0);
 				do{
-					istd::TIndex<CFastModelFinder::FEATURE_LEVELS_COUNT> modelAngleIndices;
-					for (int i = 0; i < CFastModelFinder::FEATURE_LEVELS_COUNT; ++i){
+					istd::TIndex<SpaceDimension> modelAngleIndices;
+					for (int i = 0; i < SpaceDimension; ++i){
 						modelAngleIndices[i] = (imageAngleIndices[i] + angleDeltaIndices[i] + m_spaceSizes[i] - m_leftTolerance) % m_spaceSizes[i];
 					}
 
@@ -81,9 +82,29 @@ private:
 };
 
 
+// public methods
+
+template <int SpaceDimension>
+TNeighbourBinCaller<SpaceDimension>::TNeighbourBinCaller()
+:	m_spaceSizes(),
+	m_leftTolerance(0),
+	m_rightTolerance(0)
+{
+}
+
+
+template <int SpaceDimension>
+void TNeighbourBinCaller<SpaceDimension>::InitForCyclicSpace(const istd::CVarIndex& spaceSizes, int leftTolerance, int rightTolerance)
+{
+	m_spaceSizes = spaceSizes;
+	m_leftTolerance = leftTolerance;
+	m_rightTolerance = rightTolerance;
+}
+
+
 } // namespace ialgo
 
 
-#endif // !ialgo_CNeighbourBinCaller_included
+#endif // !ialgo_TNeighbourBinCaller_included
 
 
