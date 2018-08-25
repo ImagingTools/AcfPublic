36d0445f9b79e6ba51d12dd5a97af9fccb00fbe8
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2018-08-25 14:26:48 +0000

    Automatic update from Build Server ACF version 2.0.3.120 (4710) ACF-Solutions version 2.1.2.67 (1995) IACF version 0.4.1.51 (818)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2534 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/i2d/CObject2dFactory.cpp
+++ Acf/Include/i2d/CObject2dFactory.cpp
@@ -25,7 +25,16 @@
 
 // ACF includes
 #include <imod/TModelWrap.h>
+
 #include <i2d/CLine2d.h>
+#include <i2d/CCircle.h>
+#include <i2d/CAnnulus.h>
+#include <i2d/CAnnulusSegment.h>
+#include <i2d/CRectangle.h>
+#include <i2d/CPolypoint.h>
+#include <i2d/CPolyline.h>
+#include <i2d/CPolygon.h>
+#include <i2d/CPosition2d.h>
 
 
 namespace i2d
@@ -33,6 +42,14 @@ namespace i2d
 
 
 static QByteArray s_line2dTypeId = CLine2d::GetTypeName();
+static QByteArray s_circle2dTypeId = CCircle::GetTypeName();
+static QByteArray s_ring2dTypeId = CAnnulus::GetTypeName();
+static QByteArray s_ringSegment2dTypeId = CAnnulusSegment::GetTypeName();
+static QByteArray s_rect2dTypeId = CRectangle::GetTypeName();
+static QByteArray s_polypoint2dTypeId = CPolypoint::GetTypeName();
+static QByteArray s_polyline2dTypeId = CPolyline::GetTypeName();
+static QByteArray s_polygon2dTypeId = CPolygon::GetTypeName();
+static QByteArray s_position2dTypeId = CPosition2d::GetTypeName();
 
 
 // reimplemented (istd::TIFactory<iser::IObject>)
@@ -41,6 +58,30 @@ iser::IObject* CObject2dFactory::CreateInstance(const QByteArray& keyId) const
 {
 	if (keyId == s_line2dTypeId){
 		return new imod::TModelWrap<CLine2d>();
+	}
+	else if (keyId == s_position2dTypeId){
+		return new imod::TModelWrap<CPosition2d>();
+	}
+	else if (keyId == s_polypoint2dTypeId){
+		return new imod::TModelWrap<CPolypoint>();
+	}
+	else if (keyId == s_polyline2dTypeId){
+		return new imod::TModelWrap<CPolyline>();
+	}
+	else if (keyId == s_polygon2dTypeId){
+		return new imod::TModelWrap<CPolygon>();
+	}
+	else if (keyId == s_circle2dTypeId){
+		return new imod::TModelWrap<CCircle>();
+	}
+	else if (keyId == s_ring2dTypeId){
+		return new imod::TModelWrap<CAnnulus>();
+	}
+	else if (keyId == s_ringSegment2dTypeId){
+		return new imod::TModelWrap<CAnnulusSegment>();
+	}
+	else if (keyId == s_rect2dTypeId){
+		return new imod::TModelWrap<CRectangle>();
 	}
 	else{
 		return NULL;
