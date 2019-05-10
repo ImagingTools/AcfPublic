5944cbde6cc3ef76e866826916621e507ac40ff7
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2019-05-10 16:27:03 +0000

    Build script for VS 2017 added
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2622 7b0a5285-9844-ea4a-88f9-6912f79b293c

---
+++ Build/VC15_64/_BuildNMake.bat
@@ -0,0 +1,10 @@
+call "%VS_2017_ROOT%\VC\Auxiliary\Build\vcvars64.bat"
+set COMPILER_EXT=VC15_64
+echo %QTDIR%
+set QMAKESPEC=%QTDIR%\mkspecs\win32-msvc
+set path=%path%;%QTDIR%\bin
+cd %~dp0\..\..
+cd Build\QMake
+%QTDIR%\bin\qmake AcfPublicAll.pro
+nmake
+nmake debug
