9caabf236a1d6fc0d41fbd5953ad578f77569d3f
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2019-05-18 09:00:48 +0000

    Time stamp based file name generation made more flexible
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2627 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/ifile/CTimeStampFileNameParamComp.cpp
+++ Acf/Include/ifile/CTimeStampFileNameParamComp.cpp
@@ -37,9 +37,18 @@ namespace ifile
 
 const QString& CTimeStampFileNameParamComp::GetPath() const
 {
-	QString generatedPath = QDate::currentDate().toString("yyyyMMdd");
+	const QString format = m_dateFormatAttrPtr.IsValid() ? *m_dateFormatAttrPtr : "yyyyMMdd";
+	const QString prefix = m_prefixAttrPtr.IsValid() ? *m_prefixAttrPtr : "";
+	const QString suffix = m_suffixAttrPtr.IsValid() ? *m_suffixAttrPtr : "";
 
-	m_generatedPath = BaseClass::GetPath() + "/" + generatedPath;
+	QString generatedPath = QDate::currentDate().toString(format);
+
+	m_generatedPath = BaseClass::GetPath() + "/" + prefix + generatedPath + suffix;
+
+	if (GetPathType() == PT_FILE){
+		QString extension = m_fileExtensionAttrPtr.IsValid() ? *m_fileExtensionAttrPtr : "";
+		m_generatedPath += extension;
+	}
 
 	return m_generatedPath;
 }
