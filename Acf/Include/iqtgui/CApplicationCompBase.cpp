591686079bb45ede804a4c12d1f243ed38892755
kl <kl@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2020-05-12 16:22:41 +0000

    Pending changes
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2725 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/iqtgui/CApplicationCompBase.cpp
+++ Acf/Include/iqtgui/CApplicationCompBase.cpp
@@ -133,7 +133,7 @@ bool CApplicationCompBase::TryShowSplashScreen()
 		m_applicationPtr->processEvents(QEventLoop::ExcludeUserInputEvents);
 #endif
 		QEventLoop loop;
-		QTimer::singleShot(*m_splashTimeAttrPtr, &loop, &QEventLoop::quit);
+		QTimer::singleShot(*m_splashTimeAttrPtr * 1000, &loop, &QEventLoop::quit);
 		loop.exec();
 
 		return true;
