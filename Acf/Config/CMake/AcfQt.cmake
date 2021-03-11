e86628570be61bf0eef067c92a0a938dbf7bf175
vs <vs@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2021-03-11 09:38:08 +0000

    Improved CMake files
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2790 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Config/CMake/AcfQt.cmake
+++ Acf/Config/CMake/AcfQt.cmake
@@ -8,10 +8,10 @@ link_directories(${ACFSLNLIBDIR})
 
 #link_libraries(iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
 #target_link_libraries(${PROJECT_NAME} -liqtdoc -liview -liqt2d -liqtprm -liloggui -lifilegui -liqtgui -liwidgets)
-acf_link_libraries(${PROJECT_NAME} iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
+#acf_link_libraries(${PROJECT_NAME} iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
 
 
-#target_link_libraries(${PROJECT_NAME} iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
+target_link_libraries(${PROJECT_NAME} iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
 
 #list(APPEND LIST_LIBRARY iqtdoc iview iqt2d iqtprm iloggui ifilegui iqtgui iwidgets)
 #list(APPEND LIST_LIBRARY iqtdoc)
