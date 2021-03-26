b713cd42d372858bb369b758e120d0d50e31ae76
vs <vs@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2021-03-26 10:38:13 +0000

    Improved CMake files
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2803 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- IAcf/Config/CMake/Qwt3d.cmake
+++ IAcf/Config/CMake/Qwt3d.cmake
@@ -2,5 +2,5 @@ include_directories("$ENV{QWT3DDIR_0_3_1}/include")
 #INCLUDEPATH += "$(QWT3DDIR_0_3_1)/Include"
 target_link_directories(${PROJECT_NAME} PUBLIC $ENV{QWT3DDIR_0_3_1}/Lib/${TARGETNAME})
 
-acf_link_libraries(${PROJECT_NAME} qwt3d)
+target_link_libraries(${PROJECT_NAME} qwt3d)
 #LIBS += -L$(QWT3DDIR_0_3_1)/Lib/$$COMPILER_DIR -lqwt3d
