47b799c4097f164be3a0ef821d6b4ed722cc5508
vs <vs@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2021-03-30 10:22:01 +0000

    Improved CMake files
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2811 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Config/CMake/WindeployQt.cmake
+++ Acf/Config/CMake/WindeployQt.cmake
@@ -5,6 +5,8 @@ find_package(Qt5Core REQUIRED)
 get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
 get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
 
+message("_qt_bin_dir " ${_qt_bin_dir}/windeployqt.exe)
+
 function(windeployqt target)
 
     # POST_BUILD step
@@ -12,7 +14,9 @@ function(windeployqt target)
     # - we run windeployqt on target and deploy Qt libs
 
     add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${_qt_bin_dir}/windeployqt.exe"         
+		COMMAND "${Qt5_DIR}/../../../bin/windeployqt.exe"
+#COMMAND "${_qt_bin_dir}/windeployqt.exe"
+#ARGS --verbose 1 ${AUX_INCLUDE_DIR}/../../../Bin/${TARGETNAME}
                 --verbose 1
 #                --release
 #                --no-svg
@@ -22,8 +26,9 @@ function(windeployqt target)
 #                --no-compiler-runtime
 #                --no-system-d3d-compiler
 #                \"$<TARGET_FILE:${target}>\"
-                "${AUX_INCLUDE_DIR}/../../../Bin/${TARGETNAME}"
-        COMMENT "Deploying Qt libraries using windeployqt for compilation target '${target}' ..."
+                \"${AUX_INCLUDE_DIR}/../../../Bin/${TARGETNAME}\"
+
+				COMMENT "Deploying Qt libraries using windeployqt for compilation target '${target}' ..."
     )
 
 endfunction()
