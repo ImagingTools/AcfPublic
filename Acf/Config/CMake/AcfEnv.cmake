cfab5eb65165f0830022e4a6de7ab2d21cf94721
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2022-01-27 13:13:00 +0000

    Automatic update from Build Server ACF version 2.0.3.367 (4957) ACF-Solutions version 2.1.2.224 (2152) IACF version 0.4.1.116 (883)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@2924 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Config/CMake/AcfEnv.cmake
+++ Acf/Config/CMake/AcfEnv.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.1.0)
 
-set(QT_VERSION_MAJOR 5)
+set(QT_DEFAULT_MAJOR_VERSION 5)
 
 message("CMAKE_VERSION " ${CMAKE_VERSION})
 
@@ -22,15 +22,15 @@ endif()
 if(NOT DEFINED ACFCONFIGDIR)
 	set(ACFCONFIGDIR "$ENV{ACFCONFIGDIR}")
 endif()
-if(NOT DEFINED OPENCVDIR_4_5_3)
-        set(OPENCVDIR_4_5_3 "${ACFDIR}/../3rdParty/OpenCV/4.5.3")
+if(NOT DEFINED OPENCVDIR_3_3_1)
+	set(OPENCVDIR_3_3_1 "${ACFDIR}/../3rdParty/OpenCV/3.3.1")
 endif()
 
-include("${ACFDIR}/Config/CMake/ProjectRoot.cmake")
+include("${ACFDIR}/Config/CMake/Macro.cmake")
 
 get_target_name(TARGETNAME)
 
-include_directories("${ACFDIR}/AuxInclude/Qt${QT_VERSION_MAJOR}_${TARGETNAME}")
+include_directories("${ACFDIR}/AuxInclude/Qt${QT_DEFAULT_MAJOR_VERSION}_${TARGETNAME}")
 include_directories("${ACFDIR}/Include")
 include_directories("${ACFDIR}/Impl")
 
