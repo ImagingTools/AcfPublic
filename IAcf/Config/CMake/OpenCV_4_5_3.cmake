c92eeef4ab0656e8a1ecb3ddc47c99247b230f99
Roman.Kucherenko <Roman.Kucherenko@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2022-11-14 12:16:50 +0000

    OpenCV_4_5_3.cmake corrected
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@3006 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- IAcf/Config/CMake/OpenCV_4_5_3.cmake
+++ IAcf/Config/CMake/OpenCV_4_5_3.cmake
@@ -1,7 +1,8 @@
 include_directories("$ENV{OPENCVDIR_4_5_3}/include")
 
 if(WIN32)
-        if(CMAKE_BUILD_TYPE EQUAL "Release")
+	message("OpenCV CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE}")
+	    if(CMAKE_BUILD_TYPE STREQUAL "Release")
 			target_link_libraries(${PROJECT_NAME} $ENV{OPENCVDIR_4_5_3}/lib/${CMAKE_BUILD_TYPE}_${COMPILER_CODE}/opencv_world453.lib)
     else()
 		target_link_libraries(${PROJECT_NAME} $ENV{OPENCVDIR_4_5_3}/lib/${CMAKE_BUILD_TYPE}_${COMPILER_CODE}/opencv_world453d.lib)
