3b6669d7ad0fb82daf61bfcedd9ddb0fae7eb6cc
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2022-12-26 21:24:57 +0000

    Automatic update from Build Server ACF version 2.0.3.454 (5044) ACF-Solutions version 2.1.2.250 (2178) IACF version 0.4.1.129 (896)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@3018 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Include/istd/AcfVersion.h
+++ Acf/Include/istd/AcfVersion.h
@@ -32,7 +32,7 @@ namespace istd
 */
 enum RepositoryState
 {
-	RS_ORIGINAL_VERSION =  5043,
+	RS_ORIGINAL_VERSION =  5044,
 	RS_DIRTY_FLAG = 0,
 	RS_USE_VERSION = RS_ORIGINAL_VERSION + RS_DIRTY_FLAG
 };
