9bbcafb76a9d2566b13d2f8d2457cabf55038fcb
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2023-08-16 16:32:10 +0000

    Automatic update from Build Server ACF version 2.0.3.503 (5093) ACF-Solutions version 2.1.2.266 (2194) IACF version 0.4.1.136 (903)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@3104 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Config/CMake/AcfStdGui.cmake
+++ Acf/Config/CMake/AcfStdGui.cmake
@@ -1,3 +1,3 @@
 #target_link_libraries(${PROJECT_NAME} iqt ipackage idoc ifile iimg icmm i2d i3d iprm ilog ibase icomp iattr iser imod imath istd)
 
-target_link_libraries(${PROJECT_NAME} ipackage icmm i3d iprm ilog ibase icomp iattr iser imod imath istd)
+target_link_libraries(${PROJECT_NAME} iqt idoc ifile iimg i2d)
