a2520996c099c3df51355d2b483a4b83326d33da
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2023-08-21 06:57:17 +0000

    Automatic update from Build Server ACF version 2.0.3.505 (5095) ACF-Solutions version 2.1.2.266 (2194) IACF version 0.4.1.137 (904)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@3107 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- Acf/Config/CMake/AcfStd.cmake
+++ Acf/Config/CMake/AcfStd.cmake
@@ -1,3 +1,1 @@
-#target_link_libraries(${PROJECT_NAME} iqt ipackage idoc ifile iimg icmm i2d i3d iprm ilog ibase icomp iattr iser imod imath istd)
-
-target_link_libraries(${PROJECT_NAME} ipackage icmm i3d iprm ilog ibase icomp iattr iser imod imath istd)
+target_link_libraries(${PROJECT_NAME} iqt ipackage icmm i3d iprm ilog ibase icomp iattr iser imod imath istd)
--- Acf/Config/CMake/AcfStdGui.cmake
+++ Acf/Config/CMake/AcfStdGui.cmake
@@ -1,3 +1,1 @@
-#target_link_libraries(${PROJECT_NAME} iqt ipackage idoc ifile iimg icmm i2d i3d iprm ilog ibase icomp iattr iser imod imath istd)
-
-target_link_libraries(${PROJECT_NAME} iqt idoc ifile iimg i2d)
+target_link_libraries(${PROJECT_NAME} idoc ifile iimg i2d)
