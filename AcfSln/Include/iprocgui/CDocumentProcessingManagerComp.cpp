5eb5457cc71219ef28e41b3a31e2767fa61d7783
acf <acf@7b0a5285-9844-ea4a-88f9-6912f79b293c>
2025-03-24 00:39:44 +0000

    Automatic update from Build Server ACF version 2.0.3.756 (5346) ACF-Solutions version 2.1.2.308 (2236) IACF version 0.4.1.153 (920)
    git-svn-id: http://b035a0a.online-server.cloud/svn/AcfPublic@3346 7b0a5285-9844-ea4a-88f9-6912f79b293c

--- AcfSln/Include/iprocgui/CDocumentProcessingManagerComp.cpp
+++ AcfSln/Include/iprocgui/CDocumentProcessingManagerComp.cpp
@@ -78,7 +78,7 @@ void CDocumentProcessingManagerComp::DoProcessingToOutput(const istd::IChangeabl
 
 	istd::CChangeNotifier changePtr(outputDocumentPtr);
 
-    istd::CGeneralTimeStamp timer;
+	istd::CGeneralTimeStamp timer;
 
 	int retVal = m_processorCompPtr->DoProcessing(
 				m_paramsSetCompPtr.GetPtr(),
@@ -86,7 +86,7 @@ void CDocumentProcessingManagerComp::DoProcessingToOutput(const istd::IChangeabl
 				outputDocumentPtr,
 				m_progressManagerCompPtr.GetPtr());
 
-    double processingTime = timer.GetElapsed();
+	double processingTime = timer.GetElapsed();
 
 	SendVerboseMessage(QObject::tr("Processing time: %1 ms").arg(processingTime, 2, 'f', 2), "Document processing manager");
 
@@ -127,7 +127,11 @@ void CDocumentProcessingManagerComp::DoInPlaceProcessing(istd::IChangeable* inpu
 		return;
 	}
 
-    istd::CGeneralTimeStamp timer;
+	if (m_progressManagerCompPtr.IsValid()) {
+		m_progressManagerCompPtr->ResetProgressManager();
+	}
+
+	istd::CGeneralTimeStamp timer;
 
 	int retVal = m_processorCompPtr->DoProcessing(
 				m_paramsSetCompPtr.GetPtr(),
@@ -135,9 +139,9 @@ void CDocumentProcessingManagerComp::DoInPlaceProcessing(istd::IChangeable* inpu
 				outputDocumentPtr.GetPtr(),
 				m_progressManagerCompPtr.GetPtr());
 
-    double processingTime = timer.GetElapsed();
+	double processingTime = timer.GetElapsed();
 
-    SendInfoMessage(0, QObject::tr("Processing time: %1 ms").arg(processingTime * 1000, 2, 'f', 2), "Document processing manager");
+	SendInfoMessage(0, QObject::tr("Processing time: %1 ms").arg(processingTime * 1000, 2, 'f', 2), "Document processing manager");
 
 	if (retVal != iproc::IProcessor::TS_OK){
 		SendErrorMessage(0, "Processing was failed", "Document processing manager");
